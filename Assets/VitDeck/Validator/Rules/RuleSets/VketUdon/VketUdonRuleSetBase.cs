10c410181617e003ee5fd0ff5fb9006fae036b91
m2wasabi <m2wasabi@gmail.com>
2021-03-17 01:59:38 +0900

    OfficialAssetData を継承可能にした
    # Conflicts:
    #	Assets/VitDeck/Validator/Rules/RuleSets/VketUdon/GameVket0OfficialAssetData.cs
    #	Assets/VitDeck/Validator/Rules/RuleSets/VketUdon/GameVket0RuleSet.cs

--- Assets/VitDeck/Validator/Rules/RuleSets/VketUdon/VketUdonRuleSetBase.cs
+++ Assets/VitDeck/Validator/Rules/RuleSets/VketUdon/VketUdonRuleSetBase.cs
@@ -41,7 +41,7 @@ namespace VitDeck.Validator.RuleSets
                 _officialAssetData.UdonBehaviourPrefabGUIDs);
         }
 
-        public IRule[] GetRules()
+        public virtual IRule[] GetRules()
         {
             // デフォルトで使っていたAttribute式は宣言時にconst以外のメンバーが利用できない。
             // 継承したプロパティを参照して挙動を変えることが出来ない為、直接リストを返す方式に変更した。

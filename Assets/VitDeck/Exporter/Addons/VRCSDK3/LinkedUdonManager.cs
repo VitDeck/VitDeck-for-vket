8e7eb1411764d5b14b7d0a559be712519e1fb52a
m2wasabi <m2wasabi@gmail.com>
2021-03-21 03:09:58 +0900

    [VitDeck Exporter] Add Exclude SerializedUdonProgram GUIDs

--- Assets/VitDeck/Exporter/Addons/VRCSDK3/LinkedUdonManager.cs
+++ Assets/VitDeck/Exporter/Addons/VRCSDK3/LinkedUdonManager.cs
@@ -34,6 +34,9 @@ namespace VitDeck.Exporter.Addons.VRCSDK3
             "2595ee2141e0fc4408caf75680ef9eb5",  // 0d2cf2386895ff5499a1660a4327ad75 Assets/VketAssets/UdonPrefabs/Udon_AvatarPedestal/Scripts/AvatarPedestal.asset
             "cb458453c6b8c4a4884ba5d3b2f9de56",  // 6ea1e8fa7533f9647996810212fa976e Assets/VketAssets/UdonPrefabs/Udon_CirclePageOpener/Scripts/CirclePageOpener.asset
             "4c879ab359f0cc54984884027c8a015e",  // fe9c6cf6073f2514e82259a4142d6932 Assets/VitDeck/Templates/07_UC/SharedAssets/UC_WorldSetting.asset
+            "07ddc9396861b7c4cad40254456b1f9b",  // 6280669e97dfe384988401d2c650a08f Assets/VitDeck/Templates/G00_Gamv0/SharedAssets/UdonScript/WorldSetting(Gamv0).asset
+            "f7ce97192c1898c47a87f949b891d91f",  // 414a471b5fafc3246a7c33b2ae29f2b9 Assets/GamVGimmicks/VideoPlayer_system/BoothVideoPlayer.prefab
+            "5a528a40ac7004845bcc072f255cd626",  // 6abc392e5dd9c1848b09205e404a5adf Assets/GamVGimmicks/VideoPlayer_system/BoothVideoPlayerMaster.prefab
         };
     }
 }

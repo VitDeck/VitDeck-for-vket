ef247a71478fb40cf009a310722181911218ebdb
Mech_Are_Nick <vt.212.mech@gmail.com>
2021-06-03 20:27:10 +0900

    コア・縁日のバリデーションルール及び入稿シーンの実装
    # Conflicts:
    #	Assets/VketTools/Config/WorldDefinitions/Vket6/1.asset
    #	Assets/VketTools/Config/WorldDefinitions/Vket6/2.asset

---
+++ Assets/VitDeck/Validator/Rules/RuleSets/VketUdon/Vket6/Vket6EnNichiRuleSet.cs
@@ -0,0 +1,121 @@
+#if VRC_SDK_VRCSDK3
+using System.Linq;
+using UnityEngine;
+using VitDeck.Language;
+
+namespace VitDeck.Validator.RuleSets
+{
+    public class Vket6EnNichiRuleSet : VketUdonRuleSetBase
+    {
+        public Vket6EnNichiRuleSet():base(new Vket6OfficialAssetData())
+        {
+        }
+        public override string RuleSetName => "Vket6 - EnNichi";
+
+        protected override string UdonSharpNamespace => "Vket6.Circle";
+
+        protected override long FolderSizeLimit => 100 * MegaByte;
+
+        protected override Vector3 BoothSizeLimit => new Vector3(10, 10, 10);
+
+        protected override int UdonBehaviourCountLimit => 20;
+
+        protected override int VRCObjectSyncCountLimit => 10;
+
+        protected override int VRCObjectPoolCountLimit => 1;
+
+        protected override int VRCObjectPoolPoolLimit => 5;
+
+        protected override int VRCPickupCountLimit => 20;
+
+        protected override int UdonBehaviourSynchronizePositionCountLimit => 10;
+
+        protected override int UdonScriptSyncedVariablesLimit => 10;
+
+        protected override int MaterialUsesLimit => 40;
+
+        protected override int LightmapCountLimit => 2;
+
+        protected override int VRCStationCountLimit => 8;
+
+        protected override int ClothCountLimit => 1;
+
+        protected override int AudioSourceCountLimit => 10;
+
+        protected override int VketVideoPlayerUsesLimit => 1;
+
+        protected override LightConfigRule.LightConfig ApprovedPointLightConfig
+        {
+            get
+            {
+                return new LightConfigRule.LightConfig(
+                            new[] { LightmapBakeType.Baked, LightmapBakeType.Realtime },
+                            0, 7,
+                            0, 10,
+                            0, 15);
+            }
+        }
+
+        protected override LightConfigRule.LightConfig ApprovedSpotLightConfig
+        {
+            get
+            {
+                return new LightConfigRule.LightConfig(
+                            new[] { LightmapBakeType.Baked, LightmapBakeType.Realtime },
+                            0, 7,
+                            0, 10,
+                            0, 15);
+            }
+        }
+
+        protected override LightConfigRule.LightConfig ApprovedAreaLightConfig
+        {
+            get
+            {
+                return new LightConfigRule.LightConfig(
+                            new[] { LightmapBakeType.Baked },
+                            0, 30,
+                            0, 10,
+                            0, 15);
+            }
+        }
+
+        protected override int AreaLightUsesLimit => 3;
+
+        protected override LightmapBakeType[] unusablePointLightModes
+        {
+            get
+            {
+                return new LightmapBakeType[] { LightmapBakeType.Realtime, LightmapBakeType.Mixed };
+            }
+        }
+
+        protected override LightmapBakeType[] unusableSpotLightModes
+        {
+            get
+            {
+                return new LightmapBakeType[] { LightmapBakeType.Realtime, LightmapBakeType.Mixed };
+            }
+        }
+
+        protected override ValidationLevel MoreAdvancedObjectValidationLevel => ValidationLevel.ALLOW;
+
+        protected override int ChairPrefabUsesLimit => 1;
+
+        protected override int PickupObjectSyncUsesLimit => 5;
+
+        protected override bool UdonInactiveRuleIsEnabled => false;
+
+        public override IRule[] GetRules()
+        {
+            var rules = base.GetRules().ToList();
+            // シェーダーホワイトリストのチェック
+            rules.Add(
+                new ShaderWhitelistRule(LocalizedMessage.Get("Booth.ShaderWhiteListRule.Title"),
+                    _officialAssetData.AllowedShaders, LocalizedMessage.Get("Booth.ShaderWhiteListRule.Solution")) 
+                );
+            return rules.ToArray();
+        }
+    }
+}
+#endif
